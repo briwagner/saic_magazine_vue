edcf3fd62dc4e4f81c5ef6b2fc7ec0fe27bff4d5
Brian Wagner <briwagner@outlook.com>
2018-08-21 11:55:22 -0500

    move to external js data file

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-const jsonRaw = [
-  {
-    "name": "Elisa Tenny",
-    "title": "President",
-    "text": "This is important stuff",
-    "imageFile": "/images/tenny.jpg",
-    "source": "Important photo"
-  },
-  {
-    "name": "Another person",
-    "title": "Not the president",
-    "text": "This is important stuff",
-    "imageFile": "/images/ashley.jpg",
-    "source": "Important photo"
-  },
-  {
-    "name": "A large group",
-    "title": "All sorts of people",
-    "text": "This is important stuff",
-    "imageFile": "/images/group.jpg",
-    "source": "Important photo"
-  }
-];
+// const jsonRaw = [
+//   {
+//     "name": "Elissa Tenny",
+//     "title": "President",
+//     "intro": "<p>Elissa Tenny is the 15th executive and first woman to lead SAIC in its more than 150-year history. She is a career-long champion of art and design education and the liberal arts as well as an advocate of interdisciplinary practice, diversity in higher learning, and the role higher education plays in society and the world at large.</p>",
+//     "text": "<p><strong>What have the artists and designers at SAIC taught you?</strong></p><p>You learn more when exploring and iterating a problem (or painting or paper) than in rushing toward a solution.</p><p><strong>Something only artists can do?</strong></p><p>Artists are not separate from society. Rather, the artist is a citizen with the same obligation as every other citizen: to use their expertise to participate in the world. What is unique about the role of the artist and designer is that their expertise has to do with vision, meaning both what we look at aesthetically and the meaning that artists reveal. The visionary creativity of artists—to be able to see something before it is there— is essential to how humanity solves its problems.</p><p><strong>SAIC’s most significant contribution to Chicago?</strong></p><p>Our students and alums. They live here, many long after graduation, making and exhibiting art, adding to our experience—both in contemplation and in play— of this great city. Our students and alums keep Chicago a dynamic, engaged center for art and design.</p>",
+//     "filename": "tenny.jpg",
+//     "quote": "Our campaign is a direct result of the entire SAIC family coming together."
+//   },
+//   {
+//     "name": "Mark Jeffery",
+//     "title": "Associate Professor, Auction Artist",
+//     "text": "<p><strong>What does faculty support mean to you?</strong></p><p>The campaign dollars will allow faculty to develop our work and to showcase it locally, nationally, and internationally.</p>    <p><strong>Best thing about SAIC?</strong></p>    <p>It keeps me relevant and always with questions that sometimes don’t have answers.</p>    <p><strong>Dream collaborator?</strong></p>    <p>Madonna. She looks at what is happening right now, moves the culture forward, and as a feminist, gives artists permission to grow and take risks.</p>",
+//     "filename": "jeffery.jpg",
+//     "source": "thing to see"
+//   },
+//   {
+//     "name": "Aram Han Sifuentes (MFA 2013)",
+//     "title": "Auction Artist",
+//     "text": "Describe your art practice.\r\nAt the core of my practice, I create socially engaged and materially rich projects that aim to create an art world that is available and accessible for those who are disenfranchised, particularly for dispossessed immigrants of color.",
+//     "filename": "sifuentes.jpg",
+//     "quote": "Sewing is my medium to investigate identity politics, immigration and immigrant labor, possession and dispossession, citizenship and belonging, dissent and protest, and race politics in the United States."
+//   }
+// ];
+
+rawData.map((x) => x.filename = 'images/' + x.filename)
 
 const ImageGrid = {
   name: 'image-grid',
@@ -46,32 +49,35 @@ const ImageGrid = {
       <div v-for="(item, index) in gridItems"
            v-on:click="makeFeatured(index)"
            class="grid-item"
-           v-bind:style="{backgroundImage: 'url(' + item.imageFile + ')' }">
+           v-bind:style="{backgroundImage: 'url(' + item.filename + ')' }">
         <p class="img-source">{{ item.source }}</p>
       </div>
     </div>
     <div class="grid-featured" v-show="featuredItem">
       <p>{{ featuredItem.name }}</p>
       <p>{{ featuredItem.title }}</p>
-      <p>{{ featuredItem.questions }}</p>
+      <div v-html="featuredItem.text"> </div>
+      <p class="image-source" v-if="featuredItem.source">Source: {{featuredItem.source}}</p>
     </div>
   </div>
   `
 };
 
-// 80-20 item ... allow for reversed?
+// 80-20 item
 const LandscapeRow = {
   name: 'landscape-row',
   props: ['item'],
   template:`
   <div class="landscape-row">
     <div class="row-img">
-      <img v-bind:src="item.imageFile" />
+      <img v-bind:src="item.filename" />
     </div>
     <div class="row-text">
       <h4>{{item.name}}</h4>
       <p><strong>{{item.title}}</strong></p>
-      <p>{{item.questions}}</p>
+      <div v-html="item.text"> </div>
+      <p class="quote-block">{{item.quote}}</p>
+      <p class="image-source" v-if="item.source">Source: {{item.source}}</p>
     </div>
   </div>
   `
@@ -90,16 +96,17 @@ const HalfHalfRow = {
   <div class="half-row">
     <div v-if="position != 'right'"
          class="row-img">
-      <img v-bind:src="item.imageFile" />
+      <img v-bind:src="item.filename" />
     </div>
     <div class="row-text">
       <h4>{{item.name}}</h4>
       <p><strong>{{item.title}}</strong></p>
-      <p>{{item.questions}}</p>
+      <div v-html="item.text"> </div>
+      <p class="image-source" v-if="item.source">Source: {{item.source}}</p>
     </div>
     <div v-if="position == 'right'"
          class="row-img">
-      <img v-bind:src="item.imageFile" />
+      <img v-bind:src="item.filename" />
     </div>
   </div>
   `
@@ -112,12 +119,13 @@ const PortraitRow = {
   template: `
   <div class="portrait-row">
     <div class="row-img">
-      <img v-bind:src="item.imageFile" />
+      <img v-bind:src="item.filename" />
     </div>
     <div class="row-text">
       <h4>{{item.name}}</h4>
       <p><strong>{{item.title}}</strong></p>
-      <p>{{item.questions}}</p>
+      <div v-html="item.text"> </div>
+      <p class="image-source" v-if="item.source">Source: {{item.source}}</p>
     </div>
   </div>
   `
@@ -136,12 +144,19 @@ const App = {
     HalfHalfRow,
     PortraitRow
   },
+  computed: {
+    firstGrid: function() {
+      let gridItems = rawData.slice(2, 6);
+      return gridItems;
+    }
+  },
   template: `
   <div>
-    <portrait-row :item="jsonRaw[0]" />
-    <landscape-row :item="jsonRaw[2]" />
-    <image-grid :gridItems="jsonRaw" />
-    <half-half-row :item="jsonRaw[1]" :position="'right'"/>
+    <portrait-row :item="rawData[0]" />
+    <landscape-row :item="rawData[1]" />
+    <image-grid :gridItems="firstGrid" />
+    <half-half-row :item="rawData[2]" :position="'right'"/>
+    <half-half-row :item="rawData[3]"/>
   </div>`
 }
 
---
+++ rawData.js
@@ -0,0 +1,45 @@
+const rawData = [
+{
+  "name": "Elissa Tenny",
+  "title": "President",
+  "intro": "<p>Elissa Tenny is the 15th executive and first woman to lead SAIC in its more than 150-year history. She is a career-long champion of art and design education and the liberal arts as well as an advocate of interdisciplinary practice, diversity in higher learning, and the role higher education plays in society and the world at large.</p>",
+  "text": "<p><strong>What have the artists and designers at SAIC taught you?</strong></p><p>You learn more when exploring and iterating a problem (or painting or paper) than in rushing toward a solution.</p><p><strong>Something only artists can do?</strong></p><p>Artists are not separate from society. Rather, the artist is a citizen with the same obligation as every other citizen: to use their expertise to participate in the world. What is unique about the role of the artist and designer is that their expertise has to do with vision, meaning both what we look at aesthetically and the meaning that artists reveal. The visionary creativity of artists—to be able to see something before it is there— is essential to how humanity solves its problems.</p><p><strong>SAIC’s most significant contribution to Chicago?</strong></p><p>Our students and alums. They live here, many long after graduation, making and exhibiting art, adding to our experience—both in contemplation and in play— of this great city. Our students and alums keep Chicago a dynamic, engaged center for art and design.</p>",
+  "filename": "tenny.jpg",
+  "quote": "Our campaign is a direct result of the entire SAIC family coming together."
+},
+{
+  "name": "Mark Jeffery",
+  "title": "Associate Professor, Auction Artist",
+  "text": "<p><strong>What does faculty support mean to you?</strong></p><p>The campaign dollars will allow faculty to develop our work and to showcase it locally, nationally, and internationally.</p><p><strong>Best thing about SAIC?</strong></p><p>It keeps me relevant and always with questions that sometimes don’t have answers.</p><p><strong>Dream collaborator?</strong></p><p>Madonna. She looks at what is happening right now, moves the culture forward, and as a feminist, gives artists permission to grow and take risks.</p>",
+  "filename": "jeffery.jpg"
+},
+{
+  "name": "Aram Han Sifuentes (MFA 2013)",
+  "title": "Auction Artist",
+  "text": "<p><strong>Describe your art practice.</strong></p><p>At the core of my practice, I create socially engaged and materially rich projects that aim to create an art world that is available and accessible for those who are disenfranchised, particularly for dispossessed immigrants of color.</p>",  "quote": "Sewing is my medium to investigate identity politics, immigration and immigrant labor, possession and dispossession, citizenship and belonging, dissent and protest, and race politics in the United States.",
+  "source": "Virginal Harold, Pulitzer Arts Foundation",
+  "filename": "sifuentes.jpg"
+},
+{
+  "name": "Ellen Sandor (MFA 1975, HON 2014)",
+  "title": "Board of Governors",
+  "text": "<p><strong>What excites you most right now?</strong></p><p>The privilege of being able to do my art every day.</p>",
+  "quote": "Art saves, and tough art really saves.",
+  "source": "Amy Creyer",
+  "filename": "sandor.jpg"
+},
+{
+  "name": "Erin Pierce",
+  "title": "MakeWork Council Member",
+  "text": "<p><strong>How do scholarships transform students’ lives?</strong></p><p>I’ve heard stories from many artists who might not have been able to go to SAIC at all had it not been for a scholarship grant.</p><p><strong>What have the artists and designers at SAIC taught you?</strong></p><p>Most significantly, creating requires being brave and being open. It does not always require genius. It requires intense dedication.</p><p><strong>What makes SAIC a world leader in art and design education?</strong></p><p>The passion is palpable. The thinking is robust and broad. It’s energizing, and it feeds my imagination even through in-passing interactions.</p>",
+  "source": "Jen Dean",
+  "filename": "pierce.jpg"
+},
+{
+  "name": "Renluka Maharaj (MFA 2017)",
+  "title": "Daniel Berger Barbara DeGenevieve Graduate Merit Scholarship Recipient",
+  "text": "<p><strong>Describe your art practice.</strong></p>I employ personal narratives from family members and friends, scholars, and religious persons.<p><strong>Why do you support SAIC?</strong></p>I support SAIC because I felt E so supported as a student,and I wish to give that back.<p><strong>Advice to your younger self?</strong></p>Pursue what you love, not what makes you the most money.",
+  "quote": "The best advice was from a visiting artist who told me to stop looking at my work through the eye of a colonizer.",
+  "filename": "maharaj.jpg"
+}
+]
